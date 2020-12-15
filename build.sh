993e700e229be0cb6b15eb3716f24ddba00106dc
lance <net21st@gmail.com>
2020-12-15 16:41:46 +0000

    build.sh : add host <-> container shared /tmp dir

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-CONTAINERNAME=devcon
 
 grep -e "NEWUSER=\w" Dockerfile
 
@@ -10,21 +9,27 @@ then
 	exit 1;
 fi
 
+
+CONTAINERNAME=devcon
 echo "### container name set to: $CONTAINERNAME"
 
-docker build ./ --tag code
+mkdir -p /tmp/$CONTAINERNAME
+echo "### created directory in /tmp/$CONTAINERNAME"
 
+
+docker build ./ --tag code
 echo "### image built"
 
 export DOCKERGID=`cat /etc/group | grep docker | cut -d ':' -f 3`
-
 echo "### Docker group ID detected as: $DOCKERGID"
+
 
 echo "### starting container ..."
 
 docker run 	--mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock 	\
 		--mount type=bind,src=`pwd`/random.txt,dst=/var/run/random.txt,readonly	\
-		--hostname $CONTAINERNAME --name $CONTAINERNAME --env DOCKERGID 	\
+		--mount type=bind,src=/tmp/$CONTAINERNAME,dst=/tmp/$CONTAINERNAME	\
+		--name $CONTAINERNAME --hostname $CONTAINERNAME --env DOCKERGID 	\
 		--interactive --tty 							\
 		  code
 
