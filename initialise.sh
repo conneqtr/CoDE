fad736bd9d635b7ed1c130ad5c14fba1a3bd0f0e
lance@aster <lance@villagesoftware.co.uk>
2020-12-23 17:10:06 +0000

    initialise.sh - pacman sync, vimplug, ungit and stronger ownership transfer

--- initialise.sh
+++ initialise.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+VIMPLUGURL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+pacman --sync --refresh
+pacman --sync --sysupgrade --noconfirm # the image may have been built with cached layers ...
+echo "all installed packages upgraded" # ... therefore some dependencies may be stale causing issues
 
 groupmod --gid $DOCKERGID docker
 chgrp $DOCKERGID /var/run/docker.sock
@@ -15,17 +20,26 @@ rm -rf .*   # remove any spurious config files inherited from build environ
 if [ -d dotfiles ]; then ./dotfiles/install.sh; fi;
 echo "### installed config files for user '$NEWUSER'"
 
+curl -v -fLo .vim/autoload/plug.vim  --create-dirs $VIMPLUGURL
+echo "### installed vimplug"
+
+npm install -g ungit
+echo "### installed ungit"
+
 rm -rf *
 echo "### cleaned home directory"
 
 mkdir projects
-chown -R $NEWUSER:users ./*
 echo "### created projects directory"
+
+chown -R $NEWUSER:users ./*
+chown -R $NEWUSER:users ./.*   # make sure we get all the .files too
+echo "file ownership transferred to '$NEWUSER'"
 
 echo "### user '$NEWUSER' initialised "
 
 
-pacman --sync --refresh
+pacman --sync --refresh # belt and braces
 
 echo "### you may now wish to update your packages - run 'pacman --sync --sysupgrade' as root"
 
