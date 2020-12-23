0d7afa434c09dd43481f63eb6ff7200cf3396f76
lance@aster <lance@villagesoftware.co.uk>
2020-12-23 08:41:32 +0000

    dotfiles - add tmux configuration

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -5,4 +5,8 @@ mv /home/$NEWUSER/dotfiles/dot.config /home/$NEWUSER/.config
 mv /home/$NEWUSER/dotfiles/dot.finder /home/$NEWUSER/.finder
 mv /home/$NEWUSER/dotfiles/dot.vim /home/$NEWUSER/.vim 
 mv /home/$NEWUSER/dotfiles/dot.gitconfig /home/$NEWUSER/.gitconfig
+mv /home/$NEWUSER/dotfiles/dot.tmux /home/$NEWUSER/.tmux 
+mv /home/$NEWUSER/dotfiles/dot.tmux.conf.local /home/$NEWUSER/dot.tmux.conf.local 
 
+cd /home/$NEWUSER/
+ln -s /home/$NEWUSER/.tmux/.tmux.conf
